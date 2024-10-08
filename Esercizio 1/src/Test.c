18d5c833f33dee89fc4ecaec9288dbf2968747a4
Roberto Cito <98057423+RMChannel@users.noreply.github.com>
2024-10-08 15:46:14 +0200

    Update Esercizio 1

--- Esercizio 1/src/Test.c
+++ Esercizio 1/src/Test.c
@@ -9,19 +9,36 @@
  */
 #include <ourhdr.h>
 
-int main(void) {
+int toInt(char* n) {
+	int result=0;
+	while(*n) {
+		result=(*n)-'0'+(result*10);
+		n++;
+	}
+	return result;
+}
+
+int main(int argc, char* argv[]) {
 		srand(time(NULL));
-		int fd=open("test.txt",O_CREAT | O_RDWR,0644);
+		int n;
+		if(argc==1) n=10000;
+		else n=toInt(argv[1]);
+		int fd;
+		if((fd=open("test.txt",O_CREAT | O_RDWR,0644))<0) {
+			write(2,"File non aperto/generato correttamente\n",sizeof(char)*40);
+			return -1;
+		}
 		startCounting();
-		int m, n=10000, l, count=0;
+		int m, l, count=0, temp;
 		for(int i=0;i<n;i++) {
 			m=rand()%1001;
-			char number[100]="";
-			int temp=m;
+			char number[20]="";
+			temp=m;
 			while(temp>0){
 				temp/=10;
 				count++;
 			}
+			temp=count+1;
 			if((i+1)%6==0) number[count]='\n';
 			else number[count]=' ';
 			while(m>0) {
@@ -30,8 +47,8 @@ int main(void) {
 				number[count-1]=l+'0';
 				count--;
 			}
-			write(fd,number,strlen(number));
-			printf("%s",number);
+			write(fd,number,temp);
+			write(1,number,temp);
 		}
 		close(fd);
 		stopCounting();
